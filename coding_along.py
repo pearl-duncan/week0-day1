d376e47127ab728aaaf0d6b6fd888807246fb99e
Pearl Osborne <pearlosborne@hotmail.com>
2023-08-23 21:16:05 -0500

    python loop

---
+++ coding_along.py
@@ -0,0 +1,6 @@
+name_list = ['Tucker', 'Rahiem', 'Donte', 'Dimitrius']
+for name in name_list: 
+    print(name)
+    #a for list goes step by step like a flight of staris
+    #for this ex the first step is Tucher, second is Rahiem and so on
+    print(name.upper())
