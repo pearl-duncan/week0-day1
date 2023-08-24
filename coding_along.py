91d742f913dc771d236de4c1341ffa1dc341459b
Pearl Osborne <pearlosborne@hotmail.com>
2023-08-23 21:27:21 -0500

    more python

--- coding_along.py
+++ coding_along.py
@@ -4,3 +4,20 @@ for name in name_list:
     #a for list goes step by step like a flight of staris
     #for this ex the first step is Tucher, second is Rahiem and so on
     print(name.upper())
+
+
+num1 = 9
+num2 = 9
+mult_num = num1 * num2
+print(mult_num)
+print(9*9)
+
+def mult(number1, number2):
+    print(number1 * number2)
+
+def mult2(n1, n2):
+    return n1 * n2
+
+
+
+mult(9, 9)
